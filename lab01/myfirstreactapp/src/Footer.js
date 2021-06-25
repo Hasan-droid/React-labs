af2e84e1f59714e8149ca76e3c16c28972f4cbed
hasan-droid <hasan-net2010@hotmail.com>
2021-06-25 03:04:43 +0300

    basic header main footer

---
+++ lab01/myfirstreactapp/src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div>
+               <p>&copy; ASAC 2021</p> 
+            </div>
+        )
+    }
+}
+
+export default Footer;
